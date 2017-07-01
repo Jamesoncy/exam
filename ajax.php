da1715b60f86ca9db4f78f5f1a81c63d445d9300
System Administrator <root@Jamess-MacBook-Air.local>
2017-07-01 18:36:05 +0800

    change mysql conn to mysqli

--- ajax.php
+++ ajax.php
@@ -1,31 +1,33 @@
 <?php
 include("constants.php");
-
-mysql_connect(SERVER, USER, PASSWORD) or die();
-mysql_select_db(DBNAME);
+$conn = new mysqli(SERVER, USER, PASSWORD, DBNAME);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
 $directory = "uploads/";
 
 $offset = is_numeric($_POST['offset']) ? $_POST['offset'] : die();
 $postnumbers = is_numeric($_POST['number']) ? $_POST['number'] : die();
-$run = mysql_query("SELECT * FROM picture ORDER BY id DESC LIMIT ".$postnumbers." OFFSET ".$offset);
-
+$sql = "SELECT * FROM picture ORDER BY id DESC LIMIT ".$postnumbers." OFFSET ".$offset;
+$result = $conn->query($sql);
 ?>
-    <div class="portfolio portfolio-masonry col-3 gutter">
-      
-        <?php
-            while($row = mysql_fetch_array($run)) {
+<div class="portfolio portfolio-masonry col-2 gutter">
+<?php
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
                 $id = $row["unique_id"];
-	            $image_src = $directory.$row["unique_id"].".".$row["filetype"];
+            $image_src = $directory.$row["unique_id"].".".$row["filetype"];
                 $file_name  = $row["filename"];
                 $height = $row["height"];
                 $width = $row["width"];
-	            $tags = $row["tags"];
+            $tags = $row["tags"];
                 $caption = "Filename: ".$file_name. " Height: ".$height. " Width: ". $width;
-        ?>
-	                <div class="portfolio-item cat2 cat4">
+?>
+                <div class="portfolio-item cat2 cat4">
                         <div class="thumb">
-                            <img src="<?php echo $image_src;?>" alt="" class = 'photo' style ="height: 300px !important">
+                            <img src="<?php echo $image_src;?>" class  = "photo" alt="" style ="height: 300px !important">
                             <div class = "portfolio-hover">
                                 <div class="action-btn">
                                     <a href="<?php echo $image_src;?>" data-lightbox="<?php echo $id;?>" data-title="<?php echo $caption;?>"><i class="icon-basic_magnifier"></i></a>
@@ -33,8 +35,9 @@ $run = mysql_query("SELECT * FROM picture ORDER BY id DESC LIMIT ".$postnumbers.
                             </div>
                         </div>
                         <center><p class = 'half-txt p-top-30'><?php echo $tags;?></p></center>
-                    </div>
-        <?php
-        }
-        ?>
+                    </div> 
+<?php 
+    } 
+} 
+?>
 </div>
